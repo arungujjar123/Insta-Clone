7bdfd12885da69703acda984bd202567423bc9be
Arun Gujjar <arunbadgujjar9897769094@gmail.com>
2024-08-15 02:34:27 +0530

    insta clone

---
+++ project.js
@@ -0,0 +1,29 @@
+ var istatus = document.querySelector("h5")
+
+var btn = document.querySelector("#add")
+//   var removefriend = document.querySelector("#remove")
+var check=0;
+ btn.addEventListener("click",function(){
+   if(check==0){
+     istatus.innerHTML = "Friends"
+     istatus.style.color = "green"
+     btn.innerHTML = "Remove friend"
+     check=1;
+   }
+     else{
+         istatus.innerHTML = "stranger"
+         istatus.style.color = "red"
+         btn.innerHTML = "Add friend"
+         check=0;
+     }
+    
+ })
+
+//  removefriend.addEventListener("click",function() {
+//    istatus.innerHTML = "stranger";
+//    istatus.style.color = "red";
+//  })
+
+
+// second method jisme ek sath hi remove aur add button kaam karega 
+
